67b178f60a396f8376e05917d58fc87835b7ad70
Isaiah Temitope <temitopeisaiah391@gmail.com>
2022-06-29 13:13:03 +0100

    Add files via upload

---
+++ fibonacci.py
@@ -0,0 +1,10 @@
+print('please input a number')
+num = int(input())
+def fibonacci(n):
+    if n <= 1:
+        return n
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
+for number in range(num):
+    print(fibonacci(number))
+fibonacci(num)
